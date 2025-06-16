89f4e29c59b0fd068be1c63a0749cce95bea62e9
Faruk Başer <baserfaruk.rs@gmail.com>
2025-06-16 22:30:28 +0300

    thirdv1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: 'https://rynsecurity.com',
-  integrations: [tailwind(), sitemap()],
+  site: "https://rynsecurity.com", // veya özel domain: https://rynsecurity.com
+  base: '/rynsecurity.com',   // GitHub repo adın neyse onu yaz
+  output: 'static',
+  trailingSlash: 'always',     // SEO için önerilen ayar
+  integrations: [
+    tailwind(),
+    sitemap(),
+  ],
 });
