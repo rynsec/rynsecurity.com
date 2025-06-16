c725ae1d097962c0dfc2986e830d67b7c9de9aef
Faruk Başer <baserfaruk.rs@gmail.com>
2025-06-16 12:44:05 +0300

    second

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
-import github from '@astrojs/github';
 
 export default defineConfig({
-  site: 'https://rynsecurity.com',   // kendi domainin
-  adapter: github(),                 // GitHub Pages’e uygun çıktı
+  site: 'https://rynsecurity.com',
 });
